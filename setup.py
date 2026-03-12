4f8401a14c1049ebad357017a17a564f6832d566
tecfu <help@tecfu.com>
2026-03-11 22:35:42 -0700

    fix: pip install on Python 3.12

--- setup.py
+++ setup.py
@@ -1,21 +1,8 @@
+# setup.py is now handled by pyproject.toml
+# This file is kept for backwards compatibility
 from setuptools import setup, find_packages
-
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
 
 setup(
     name="epub2tts-chatterbox",
-    description="Tool to read an epub to audiobook using chatterbox TTS",
-    author="Christopher Aedo aedo.dev",
-    author_email="c@aedo.dev",
-    url="https://github.com/aedocw/epub2tts-chatterbox",
-    license="GPL 3.0",
-    version="1.2.2",
     packages=find_packages(),
-    install_requires=requirements,
-    entry_points={
-        'console_scripts': [
-            'epub2tts-chatterbox = epub2tts_chatterbox:main'
-        ]
-    },
 )
